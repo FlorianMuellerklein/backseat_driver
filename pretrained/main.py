8c033b1d06a6fc46d5b9093f746e505c81085207
Jonathan K. Chang <JonathanKChang@gmail.com>
2016-05-18 12:32:09 -0400

    Added eval_only flag

--- pretrained/main.py
+++ pretrained/main.py
@@ -361,61 +361,66 @@ def run_cross_validation(nfolds=10, nb_epoch=10, split=0.2, modelStr=''):
         read_and_normalize_and_shuffle_train_data(img_rows, img_cols,
                                                   color_type_global)
 
-batch_aug = True # needs to be run with -p 224
-    # ishuf_train_data = []
-    # shuf_train_target = []
-    # index_shuf = range(len(train_target))
-    # shuffle(index_shuf)
-    # for i in index_shuf:
-    #     shuf_train_data.append(train_data[i])
-    #     shuf_train_target.append(train_target[i])
+    eval_only = False
+    batch_aug = True # needs to be run with -p 224
+    if not eval_only:
+        # ishuf_train_data = []
+        # shuf_train_target = []
+        # index_shuf = range(len(train_target))
+        # shuffle(index_shuf)
+        # for i in index_shuf:
+        #     shuf_train_data.append(train_data[i])
+        #     shuf_train_target.append(train_target[i])
 
-    # yfull_train = dict()
-    # yfull_test = []
-    num_fold = 0
-    kf = KFold(len(unique_drivers), n_folds=nfolds,
-               shuffle=True, random_state=random_state)
-    
-    
-    for train_drivers, test_drivers in kf:
-        num_fold += 1
-        print('Start KFold number {} from {}'.format(num_fold, nfolds))
-        # print('Split train: ', len(X_train), len(Y_train))
-        # print('Split valid: ', len(X_valid), len(Y_valid))
-        # print('Train drivers: ', unique_list_train)
-        # print('Test drivers: ', unique_list_valid)
-        # model = create_model_v1(img_rows, img_cols, color_type_global)
-        # model = vgg_bn_model(img_rows, img_cols, color_type_global)
-        model = vgg_std16_model(img_rows, img_cols, color_type_global)
+        # yfull_train = dict()
+        # yfull_test = []
+        num_fold = 0
+        kf = KFold(len(unique_drivers), n_folds=nfolds,
+                   shuffle=True, random_state=random_state)
+        
+        
+        for train_drivers, test_drivers in kf:
+            num_fold += 1
+            print('Start KFold number {} from {}'.format(num_fold, nfolds))
+            # print('Split train: ', len(X_train), len(Y_train))
+            # print('Split valid: ', len(X_valid), len(Y_valid))
+            # print('Train drivers: ', unique_list_train)
+            # print('Test drivers: ', unique_list_valid)
+            # model = create_model_v1(img_rows, img_cols, color_type_global)
+            # model = vgg_bn_model(img_rows, img_cols, color_type_global)
+            model = vgg_std16_model(img_rows, img_cols, color_type_global)
 
-        if not batch_aug:
-            model.fit(train_data, train_target, batch_size=batch_size,
-                      nb_epoch=nb_epoch,
-                      verbose=1,
-                      validation_split=split, shuffle=True)
-        else:
-            if augmentor is None:
-                augmentor = BatchAugmentor()    
-                
-            X_train, X_test, y_train, y_test = split_validation_set(train_data, train_target, test_size=split)
-            model.fit_generator(augmentor.flow(X_train, y_train, batch_size=batch_size, shuffle=True),
-                      nb_epoch=nb_epoch,
-                      verbose=1,
-                      validation_data=(X_test, y_test),
-                      samples_per_epoch=train_data.shape[0])
+            if not batch_aug:
+                model.fit(train_data, train_target, batch_size=batch_size,
+                          nb_epoch=nb_epoch,
+                          verbose=1,
+                          validation_split=split, shuffle=True)
+            else:
+                if augmentor is None:
+                    augmentor = BatchAugmentor()    
+                    
+                X_train, X_test, y_train, y_test = split_validation_set(train_data, train_target, test_size=split)
+                model.fit_generator(augmentor.flow(X_train, y_train, batch_size=batch_size, shuffle=True),
+                          nb_epoch=nb_epoch,
+                          verbose=1,
+                          validation_data=(X_test, y_test),
+                          samples_per_epoch=train_data.shape[0])
 
-        # print('losses: ' + hist.history.losses[-1])
+            # print('losses: ' + hist.history.losses[-1])
 
-        # print('Score log_loss: ', score[0])
+            # print('Score log_loss: ', score[0])
 
-        save_model(model, num_fold, modelStr)
+            save_model(model, num_fold, modelStr)
 
-        # predictions_valid = model.predict(X_valid, batch_size=128, verbose=1)
-        # score = log_loss(Y_valid, predictions_valid)
-        # print('Score log_loss: ', score)
-        # Store valid predictions
-        # for i in range(len(test_index)):
-        #    yfull_train[test_index[i]] = predictions_valid[i]
+            # predictions_valid = model.predict(X_valid, batch_size=128, verbose=1)
+            # score = log_loss(Y_valid, predictions_valid)
+            # print('Score log_loss: ', score)
+            # Store valid predictions
+            # for i in range(len(test_index)):
+            #    yfull_train[test_index[i]] = predictions_valid[i]
+
+    else:
+        num_folds = nfolds
 
     print('Start testing............')
     test_data, test_id = read_and_normalize_test_data(img_rows, img_cols,
